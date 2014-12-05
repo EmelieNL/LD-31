35d46f8b500c5d9d36ecbfea7f504b2d236b0a25
JSandmark <johansandmark@gmail.com>
2014-12-05 19:50:30 +0100

    Movment updated and working

--- SledRace/Assets/BasicSledMovment.cs
+++ SledRace/Assets/BasicSledMovment.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class BasicSledMovment : MonoBehaviour {
 
 	public int forwardSpeed = 10;
-	public int maxX = 4;
-	public int minX = -4;
+	public int maxX = 18;
+	public int minX = -18;
 
 	// Use this for initialization
 	void Start () {
@@ -14,30 +14,30 @@ public class BasicSledMovment : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(transform.position.x <= minX){
-			//transform.position = Vector3(minX, transform.position.y, transform.position.z);
-		}
 
-		if(transform.position.x >= maxX){
-			//transform.position = Vector3(maxX,transform.position.y, transform.position.z);
-		}
 		//Forward force at all time
 		rigidbody.AddForce (Vector3.forward * forwardSpeed);
 
-		//Input
-		if (Input.GetKey(KeyCode.UpArrow) ){
-			Debug.Log(transform.position);
-			//transform.position.x = minX;
-			Debug.Log("Up");
+		//Adjust movment according to player input
+		Movment();
 
-			rigidbody.AddForce (Vector3.left * 20);
-
-		} else if (Input.GetKey(KeyCode.DownArrow) ){
-			Debug.Log("Down");
-			
-			rigidbody.AddForce (Vector3.right * 20);
-			
-		}
 	
 	}
+
+	//Method to adjust player pos
+	void Movment() { 
+
+		//Read current player input movment
+		var vertical = Input.GetAxis ("Vertical"); 
+
+		//Get current position
+		Vector3 pos = transform.position;
+
+		//Set x pos with bounds
+		pos.x = Mathf.Clamp(pos.x - vertical, minX, maxX);
+
+		//update pos
+		transform.position = pos;
+		
+	} 
 }
