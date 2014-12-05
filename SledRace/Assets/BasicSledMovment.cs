d8f2c3ad1fe13629d9d8ab7a34ad36056fc9bdf9
JSandmark <johansandmark@gmail.com>
2014-12-05 19:01:00 +0100

    No Compile error with movment

--- SledRace/Assets/BasicSledMovment.cs
+++ SledRace/Assets/BasicSledMovment.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class BasicSledMovment : MonoBehaviour {
 
+	public int forwardSpeed = 10;
+	public int maxX = 4;
+	public int minX = -4;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,16 +14,30 @@ public class BasicSledMovment : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(transform.position.x <= minX){
+			//transform.position = Vector3(minX, transform.position.y, transform.position.z);
+		}
 
+		if(transform.position.x >= maxX){
+			//transform.position = Vector3(maxX,transform.position.y, transform.position.z);
+		}
 		//Forward force at all time
-		rigidbody.AddForce (Vector3.forward * 10);
+		rigidbody.AddForce (Vector3.forward * forwardSpeed);
 
 		//Input
-		if (Input.KeyDown (KeyCode.UpArrow)) {
+		if (Input.GetKey(KeyCode.UpArrow) ){
+			Debug.Log(transform.position);
+			//transform.position.x = minX;
+			Debug.Log("Up");
 
-			rigidbody.AddForce (Vector3.left * 2);
+			rigidbody.AddForce (Vector3.left * 20);
 
-				}
+		} else if (Input.GetKey(KeyCode.DownArrow) ){
+			Debug.Log("Down");
+			
+			rigidbody.AddForce (Vector3.right * 20);
+			
+		}
 	
 	}
 }
