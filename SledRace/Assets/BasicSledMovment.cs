38f9ff84bd431fa23c9909c422a6803883443c47
JSandmark <johansandmark@gmail.com>
2014-12-05 18:27:59 +0100

    Added sled test

---
+++ SledRace/Assets/BasicSledMovment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicSledMovment : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Forward force at all time
+		rigidbody.AddForce (Vector3.forward * 10);
+
+		//Input
+		if (Input.KeyDown (KeyCode.UpArrow)) {
+
+			rigidbody.AddForce (Vector3.left * 2);
+
+				}
+	
+	}
+}
